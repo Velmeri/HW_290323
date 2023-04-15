dfcb3922aa6a5da5cc41d133eab5be24b2b34938
Velmeri <danielmikhh2gmail.com>
2023-04-15 15:11:05 +0200

    Добавьте файлы проекта.

---
+++ HW_290323/Program.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace HW_290323
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			TV tv = new TV();
+			ICommand command = new TVOnCommand(tv); // specific command
+			Invoker(command, true);
+			Microwave microwave = new Microwave();
+			command = new MicrowaveCommand(microwave, 5000);
+			Invoker(command, false);
+
+			Console.ReadLine();
+		}
+
+		static void Invoker(ICommand command, bool undo)
+		{
+			Controller controller = new Controller();
+			controller.SetCommand(command);
+			controller.PressButton();
+			if (undo) {
+				controller.PressUndo();
+			}
+		}
+	}
+
+	class TV
+	{
+		public void On()
+		{
+			Console.WriteLine("TV is on!");
+		}
+
+		public void Off()
+		{
+			Console.WriteLine("TV is off!");
+		}
+	}
+
+	class Microwave
+	{
+		public void StartCooking(int time)
+		{
+			Console.WriteLine("Warming up food...");
+			System.Threading.Thread.Sleep(time);
+		}
+
+		public void StopCooking()
+		{
+			Console.WriteLine("The food is warmed up!");
+		}
+	}
+
+	interface ICommand
+	{
+		void Execute();
+		void Undo();
+	}
+
+	class TVOnCommand : ICommand
+	{
+		private TV tv;
+
+		public TVOnCommand(TV tvSet)
+		{
+			tv = tvSet;
+		}
+
+		public void Execute()
+		{
+			tv.On();
+		}
+
+		public void Undo()
+		{
+			tv.Off();
+		}
+	}
+
+	class MicrowaveCommand : ICommand
+	{
+		private Microwave microwave;
+		private int time;
+
+		public MicrowaveCommand(Microwave m, int t)
+		{
+			microwave = m;
+			time = t;
+		}
+
+		public void Execute()
+		{
+			microwave.StartCooking(time);
+			microwave.StopCooking();
+		}
+
+		public void Undo()
+		{
+			microwave.StopCooking();
+		}
+	}
+
+	class Controller
+	{
+		private ICommand command;
+
+		public void SetCommand(ICommand com)
+		{
+			command = com;
+		}
+
+		public void PressButton()
+		{
+			if (command != null) {
+				command.Execute();
+			}
+		}
+
+		public void PressUndo()
+		{
+			if (command != null) {
+				command.Undo();
+			}
+		}
+	}
+}
---
+++ HW_290323/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("HW_290323")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("HW_290323")]
+[assembly: AssemblyCopyright("Copyright © HP 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("64a6e19a-b7f7-4531-a60c-5985b89939c3")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
